54aaf26f65a8f92a2daed241ac703998ff603416
ajeetcody <ajeet.cody@gmail.com>
2021-10-17 01:25:19 +0530

    First commit

---
+++ TradersView/TradersView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TradersView
+//
+//  Created by Ajeet Sharma on 17/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
